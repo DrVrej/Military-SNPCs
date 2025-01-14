b30799159369dd47ac8468f1b6cda70eebe27fc7
DrVrej <DrVrej@users.noreply.github.com>
2025-01-14 01:59:53 -0500

    Fix tank kill icons

--- lua/entities/npc_vj_milifri_tiger_redg/shared.lua
+++ lua/entities/npc_vj_milifri_tiger_redg/shared.lua
@@ -6,3 +6,7 @@ ENT.Contact 		= "http://steamcommunity.com/groups/vrejgaming"
 ENT.Purpose 		= "Spawn it and fight with it!"
 ENT.Instructions 	= "Click on the spawnicon to spawn it."
 ENT.Category		= "Military"
+---------------------------------------------------------------------------------------------------------------------------------------------
+if CLIENT then
+	VJ.AddKillIcon("npc_vj_milifri_tiger_redg", ENT.PrintName)
+end
